f61d62335f82e2298de23a3d2996cc3aaa61115a
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-02-26 21:03:54 -0800

    add leaderboard to kills/deaths

--- commands/mostdeaths.ts
+++ commands/mostdeaths.ts
@@ -1,16 +1,10 @@
 import { Bot } from "mineflayer";
 import { PlayerDB } from "../types";
 
-export default async function mostdeaths(bot: Bot, IGN: string, args: string[], playerDB: PlayerDB) {
-	let mostDeaths = 0;
-	let mostDeathsUser = "";
+export default async function mostkills(bot: Bot, IGN: string, args: string[], playerDB: PlayerDB) {
+	const top3 = Object.entries(playerDB)
+		.sort((a, b) => b[1].deaths - a[1].deaths)
+		.slice(0, 3);
 
-	for (const user in playerDB) {
-		if (playerDB[user]["deaths"] > mostDeaths) {
-			mostDeaths = playerDB[user]["deaths"];
-			mostDeathsUser = user;
-		}
-	}
-
-	bot.sendMessage(`${mostDeathsUser} died the most with ${mostDeaths} deaths.`);
+	bot.sendMessage(`#1: ${top3[0][0]} with ${top3[0][1].deaths}, #2: ${top3[1][0]} with ${top3[1][1].deaths}, #3: ${top3[2][0]} with ${top3[2][1].deaths}`);
 }
--- commands/mostkills.ts
+++ commands/mostkills.ts
@@ -2,15 +2,9 @@ import { Bot } from "mineflayer";
 import { PlayerDB } from "../types";
 
 export default async function mostkills(bot: Bot, IGN: string, args: string[], playerDB: PlayerDB) {
-	let mostKills = 0;
-	let mostKillsUser = "";
+	const top3 = Object.entries(playerDB)
+		.sort((a, b) => b[1].kills - a[1].kills)
+		.slice(0, 3);
 
-	for (const user in playerDB) {
-		if (playerDB[user]["kills"] > mostKills) {
-			mostKills = playerDB[user]["kills"];
-			mostKillsUser = user;
-		}
-	}
-
-	bot.sendMessage(`${mostKillsUser} killed the most people with ${mostKills} kills.`);
+	bot.sendMessage(`#1: ${top3[0][0]} with ${top3[0][1].kills}, #2: ${top3[1][0]} with ${top3[1][1].kills}, #3: ${top3[2][0]} with ${top3[2][1].kills}`);
 }
