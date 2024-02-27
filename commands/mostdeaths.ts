aa090ed0c96ff2d3e31e9dc6b7f89fde30cfc40b
dsnsgithub <48170013+dsnsgithub@users.noreply.github.com>
2024-02-26 21:07:05 -0800

    make leaderboard smaller

--- commands/mostdeaths.ts
+++ commands/mostdeaths.ts
@@ -6,5 +6,5 @@ export default async function mostkills(bot: Bot, IGN: string, args: string[], p
 		.sort((a, b) => b[1].deaths - a[1].deaths)
 		.slice(0, 3);
 
-	bot.sendMessage(`#1: ${top3[0][0]} with ${top3[0][1].deaths}, #2: ${top3[1][0]} with ${top3[1][1].deaths}, #3: ${top3[2][0]} with ${top3[2][1].deaths}`);
+	bot.sendMessage(`${top3[0][0]}: ${top3[0][1].deaths}, ${top3[1][0]}: ${top3[1][1].deaths}, ${top3[2][0]}: ${top3[2][1].deaths}`);
 }
--- commands/mostkills.ts
+++ commands/mostkills.ts
@@ -6,5 +6,5 @@ export default async function mostkills(bot: Bot, IGN: string, args: string[], p
 		.sort((a, b) => b[1].kills - a[1].kills)
 		.slice(0, 3);
 
-	bot.sendMessage(`#1: ${top3[0][0]} with ${top3[0][1].kills}, #2: ${top3[1][0]} with ${top3[1][1].kills}, #3: ${top3[2][0]} with ${top3[2][1].kills}`);
+	bot.sendMessage(`${top3[0][0]}: ${top3[0][1].kills}, ${top3[1][0]}: ${top3[1][1].kills}, ${top3[2][0]}: ${top3[2][1].kills}`);
 }
